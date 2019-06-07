413cebd4fbc0da23a1b5868e901d4bb4c803bb05
mentab <25363363+mentab@users.noreply.github.com>
2019-06-07 13:23:52 +0200

    Updates

---
+++ precache-manifest.18a31bf4fbc097794b26963b2aac6298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895a8bd052d94e75aaf8ff5a9d731f76",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "3020469763b83d38f881",
+    "url": "/javascript-calculator/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/javascript-calculator/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3020469763b83d38f881",
+    "url": "/javascript-calculator/static/js/main.94d5e9b7.chunk.js"
+  },
+  {
+    "revision": "af3dc86e0aee640c7c09",
+    "url": "/javascript-calculator/static/js/runtime~main.b6bacda3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/javascript-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
