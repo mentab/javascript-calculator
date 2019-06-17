f1f672cd42062936b08d4e69473d9f41918c20f4
mentab <25363363+mentab@users.noreply.github.com>
2019-06-17 13:36:21 +0200

    start working on styles

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,10 +4,8 @@ const Display = (props) => {
 	const id = props.id;
 	const display = props.display;
 	return (
-		<div>
-        	<div id={id} class={id}>
-				{display}
-			</div>
+        <div id={id} class={id}>
+			{display}
 		</div>
     );
 };
